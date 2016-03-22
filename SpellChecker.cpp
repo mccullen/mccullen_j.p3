5010d44763996f6b758777a8e980269580a1a81c
Jeff McCullen <mccullen_j@cake.lynchburg.edu>
2016-03-22 15:53:26 -0400

    Finish editDistance() function. Needs to be re-written.

--- SpellChecker.cpp
+++ SpellChecker.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include "SpellChecker.h"
+#include <algorithm>
 /*Testing!*/
 
 /**
@@ -9,6 +10,16 @@ SpellChecker::SpellChecker(std::string lexiconFile)
 {
 }
 
+
+
+
+
+// Utility function to find minimum of three numbers
+int min2(int x, int y, int z) 
+{
+	return std::min(std::min(x, y), z);
+}
+
 /**
 Get the edit distance for two words.
 
@@ -16,10 +27,45 @@ Get the edit distance for two words.
 @param secondWord The second word to compare.
 @return The edit distance between firstWord and secondWord.
 */
-int SpellChecker::editDistance(std::string firstWord,
-	std::string secondWord)
+int SpellChecker::editDistance(std::string str1,
+	std::string str2)
 {
-	
+	const int m = str1.length();
+	const int n = str2.length();
+
+	// Create a table to store results of subproblems
+	int dp[m+1][n+1];
+ 
+	// Fill d[][] in bottom up manner
+	for (int i=0; i<=m; i++)
+	{
+		for (int j=0; j<=n; j++)
+		{
+			// If first string is empty, only option is to
+			// isnert all characters of second string
+			if (i==0)
+				dp[i][j] = j;  // Min. operations = j
+
+			// If second string is empty, only option is to
+			// remove all characters of second string
+			else if (j==0)
+				dp[i][j] = i; // Min. operations = i
+
+			// If last characters are same, ignore last char
+			// and recur for remaining string
+			else if (str1[i-1] == str2[j-1])
+				dp[i][j] = dp[i-1][j-1];
+
+			// If last character are different, consider all
+			// possibilities and find minimum
+			else
+				dp[i][j] = 1 + min2(dp[i][j-1],  // Insert
+					dp[i-1][j],  // Remove
+					dp[i-1][j-1]); // Replace
+		}
+	}
+
+	return dp[m][n];
 }
 
 /**
--- spellCheck.cpp
+++ spellCheck.cpp
@@ -6,31 +6,37 @@ Description:
 
 #include <iostream>
 #include <stdlib.h>
+#include "SpellChecker.h"
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
-	const int MAX_CMD_ARGS = 2;
-	// If the user did not enter a valid number of command line arguments,
-	// print error and exit.
-	if (argc <= MAX_CMD_ARGS)
-	{
 
-		cerr << "Error: Must have no more than "
-		     << MAX_CMD_ARGS << " command line arguments.\n";
-		exit(1);
-	}
+	int value = SpellChecker::editDistance(argv[1], argv[2]);
 
-	const int EDIT_DISTANCE_ARG = 1;
-	const int DEFAULT_EDIT_DISTANCE = 2;
-	int editDistance = DEFAULT_EDIT_DISTANCE;
+	cout << value << endl;
+	
+	//const int MAX_CMD_ARGS = 2;
+	//// If the user did not enter a valid number of command line arguments,
+	//// print error and exit.
+	//if (argc <= MAX_CMD_ARGS)
+	//{
 
-	// If the user specified the edit distance in the command line,
-	// reset the editDistance.
-	if (argc == MAX_CMD_ARGS)
-	{
-		editDistance = atoi(argv[EDIT_DISTANCE_ARG]);
-	}
+	//	cerr << "Error: Must have no more than "
+	//	     << MAX_CMD_ARGS << " command line arguments.\n";
+	//	exit(1);
+	//}
+
+	//const int EDIT_DISTANCE_ARG = 1;
+	//const int DEFAULT_EDIT_DISTANCE = 2;
+	//int editDistance = DEFAULT_EDIT_DISTANCE;
+
+	//// If the user specified the edit distance in the command line,
+	//// reset the editDistance.
+	//if (argc == MAX_CMD_ARGS)
+	//{
+	//	editDistance = atoi(argv[EDIT_DISTANCE_ARG]);
+	//}
 	
 }
